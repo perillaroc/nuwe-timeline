cc675a151ed0f14fdfffd76fee85deeb50ecf38a
perillaroc <perillaroc@gmail.com>
2016-07-18 17:15:00 +0800

    Change color.

--- test/experiment/style.js
+++ test/experiment/style.js
@@ -3,13 +3,15 @@ var color_lib = d3.schemeCategory20c;
 
 var color_domain = [
     'unknown',
-    'serial', 'serial_op',
-    'operation', 'operation1', 'operation2', 'normal', 'largemem'];
+    'serial', 'serial_op', 'operation',
+    'operation1', 'operation2', 'normal', 'largemem'
+];
+
 
 var color_range = [
         '#bdbdbd',
         '#4575b4', '#74add1',
-        '#cc4c02', '#f46d43', '#8c6bb1', '#dd3497', '#fd8d3c'
+        '#cc4c02', '#f46d43', '#feb24c', '#cb181d', '#67000d'
     ];
 
 var color_scale = d3.scaleOrdinal()
