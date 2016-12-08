bf818b5515ef135075ce31d299fd5f910feecc7d
perillaroc <perillaroc@gmail.com>
2016-12-08 20:42:34 +0800

    try to use a plain data fro timeline.

---
+++ test/manual/test1/plain.data.js
@@ -0,0 +1,63 @@
+var data = [
+    {
+        "name": "gda_gsi_v1r5",
+        "run_times": [
+            {
+                "class": "serial_op",
+                "start_time": "09:00:59",
+                "end_time": "10:01:02",
+                "name": "00",
+                "label": "640"
+            },
+            {
+                "class": "operation",
+                "start_time": "09:41:21",
+                "end_time": "09:46:10",
+                "name": "00",
+                "label": ""
+            },
+            {
+                "class": "serial_op",
+                "start_time": "13:11:00",
+                "end_time": "14:12:10",
+                "name": "06",
+                "label": "640"
+            },
+            {
+                "class": "operation",
+                "start_time": "13:51:50",
+                "end_time": "13:57:15",
+                "name": "06",
+                "label": ""
+            },
+            {
+                "class": "serial_op",
+                "start_time": "21:00:17",
+                "end_time": "21:51:23",
+                "name": "12",
+                "label": "640"
+            },
+            {
+                "class": "operation",
+                "start_time": "21:31:39",
+                "end_time": "21:36:23",
+                "name": "12",
+                "label": ""
+            },
+            {
+                "class": "serial_op",
+                "start_time": "01:10:54",
+                "end_time": "02:12:56",
+                "name": "18",
+                "label": "640"
+            },
+            {
+                "class": "operation",
+                "start_time": "01:52:46",
+                "end_time": "01:57:51",
+                "name": "18",
+                "label": ""
+            }
+        ]
+    }
+];
